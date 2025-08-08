95d700866482d760deff718253e5ec3cc0db42d0
Sujan <sujan@tonic.com.au>
2025-08-08 14:58:21 +1000

    [feat] FEAT-000: make it edge

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -29,7 +29,7 @@ export const metadata: Metadata = {
   description: "Browse all our blog posts",
 };
 
-export const dynamic = "edge";
+export const runtime = "edge";
 
 export default async function Page({
   searchParams,
